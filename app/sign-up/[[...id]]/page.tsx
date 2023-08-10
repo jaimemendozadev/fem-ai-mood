9235fb8b06b5d86c31e68c6e316de26f6f24c3a6
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2023-08-10 08:13:58 -0700

    Add props to <SignUp /> component

--- app/sign-up/[[...id]]/page.tsx
+++ app/sign-up/[[...id]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
 
 const SignUpPage = () => {
-    return <SignUp />;
+    return <SignUp afterSignUpUrl="/new-user" redirectUrl="/new-user"  />;
   }
 export default SignUpPage;
