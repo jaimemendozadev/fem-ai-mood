b09b596b35ec11c72c37abc8235f1337bfa82af2
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2023-08-25 12:16:28 -0700

    Add getEntries function to journal page

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,4 +1,25 @@
-const JournalPage = () => {
+import { getUserByClerkID } from "@/utils/auth";
+import { prisma } from "@/utils/db";
+
+const getEntries = async() => {
+    const user = await getUserByClerkID();
+
+    const entries = await prisma.journalEntry.findMany({
+        where: {
+            authorId: user.id
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+
+    return entries;
+}
+
+const JournalPage = async () => {
+    const entries = await getEntries();
+
+    console.log('entries ', entries);
     return <h1>Journal</h1>
 }
 
