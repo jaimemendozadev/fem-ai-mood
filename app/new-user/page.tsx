f194dbfa61f6ad9aa6eab7d60379cc83a7a03094
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2023-08-25 09:41:00 -0700

    Fix new user auth flow

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,34 @@
-const NewUser = () => {
-    return <h1>New User</h1>
+import {prisma} from '@/utils/db'
+import { currentUser } from '@clerk/nextjs';
+import {redirect} from 'next/navigation';
+
+const createNewUser = async () => {
+    const user = await currentUser();
+
+    console.log(user);
+
+
+
+    const match = await prisma.user.findUnique({where: {
+        clerkId: user?.id as string
+    }})
+
+    if(!match) {
+        await prisma.user.create({
+            data: {
+                clerkId: user?.id as string,
+                email: user?.emailAddresses[0].emailAddress as string
+
+            }
+        })
+    }
+
+    redirect('/journal');
+}
+
+const NewUser = async () => {
+    await createNewUser();
+    return <h1>...loading</h1>
 }
 
 export default NewUser;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
 import Link from 'next/link';
+import { auth } from '@clerk/nextjs';
 
-export default function Home() {
+export default async function Home() {
+  const {userId} = await auth();
+
+  let href = userId ? '/journal' : '/new-user';
+
+
   return (
     <main className="h-screen w-screen bg-black text-white flex justify-center items-center">
       <div className="w-full max-w-[600px] mx-auto my-auto">
@@ -11,7 +16,7 @@ export default function Home() {
         <p className="text-2xl text-white/60 mb-4">This is the best app for tracking your mood throughout your life. All you have to do is be honest.</p>
       
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">get started</button>  
           </Link>
         </div>     
