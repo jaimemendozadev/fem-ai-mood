3554c5cab36d4f7c8da7476aa38ee0935b5db8c4
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2023-08-29 08:48:47 -0700

    Copy React code for NewEntryCard
    Also copied styles for Journal Page and
    Dashboard Layout.

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -22,7 +22,7 @@ const JournalPage = async () => {
     const entries = await getEntries();
 
     console.log('entries ', entries);
-    return <div className="p-10">
+    return <div className="p-10 bg-zinc-400/10 h-full">
         <h2 className="text-3xl mb-8">Journal</h2>
 
         <div className="grid grid-cols-3 gap-4">
